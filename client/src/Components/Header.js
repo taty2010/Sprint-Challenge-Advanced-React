4f315060ea54f571a5fbcdfa4a7b68e031388ddf
taty2010 <shydalys123@gmail.com>
2020-01-26 12:46:02 -0600

    got the dark mode toggle to function by changing background and button text

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,21 +1,23 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
+import useDarkMode from './Hooks/useDarkMode';
 
 const Header = () =>{
 
     const Headers = styled.div`
         height: 40vh;
         width: 95vw;
-
+        // background-color: white;
     `;
 
 
-    const [buttonText, setButtonText] = useState('Dark Mode')
-    const [darkMode, setDarkMode] = useState(false)
+    const [buttonText, setButtonText] = useDarkMode('text','Light Mode')
+    const [darkMode, setDarkMode] = useDarkMode('bg', false)
 
     const toggleButton = e =>{
         e.preventDefault();
         setDarkMode(!darkMode)
+        setButtonText('Dark Mode')
     }
 
 
